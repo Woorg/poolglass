8e51333644306e38084ac8efb3135c27848e2326
Igor Gorlov <gorlov35@gmail.com>
2024-12-15 02:24:12 +0300

    add fallback to pll__

--- wordpress/wp-content/themes/poolglass-sage/functions.php
+++ wordpress/wp-content/themes/poolglass-sage/functions.php
@@ -3,12 +3,17 @@
 // Fallback for pll__ if Polylang is not active
 
 if (!function_exists('pll__')) {
-  function pll__($string) {
-      // Return the original string if Polylang isn't installed
-      return __($string, 'text-domain');
+  // Проверка на наличие плагина Polylang
+  if (function_exists('pll_register_string')) {
+      function pll__($string, $domain = 'sage') {
+          return \PLL()->get_language()->is_frontend() ? pll__('string', $domain) : __($string, $domain);
+      }
+  } else {
+      function pll__($string, $domain = 'sage') {
+          return __($string, $domain);
+      }
   }
 }
-
 
 /*
 |--------------------------------------------------------------------------
