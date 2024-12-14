0e5b3f7464117900864c6408692b845423d5f9ce
Igor Gorlov <gorlov35@gmail.com>
2024-12-14 23:42:21 +0300

    add multilanguage

--- wordpress/info.php
+++ wordpress/info.php
@@ -1,13 +1,20 @@
 <?php 
 
-echo '<pre>';
-print_r($_ENV);
-echo '</pre>';
-
-
 $envVars = getenv();
 
 // Вывести переменные в читаемом формате
 echo '<pre>';
 print_r($envVars);
 echo '</pre>';
+
+
+
+$mysqli = new mysqli('t4ks4c8ock884kc48wg8wgog', 'mysql', 'NOJwpgedngrJKNa99GbvoRmsfHLuAvj9Fhzhkhe9HGRyWaec6ozQJXUfz67heenh', 'default');
+
+if ($mysqli->connect_error) {
+    die('Connection failed: ' . $mysqli->connect_error);
+} else {
+    echo 'Connection successful!';
+}
+
+?>
