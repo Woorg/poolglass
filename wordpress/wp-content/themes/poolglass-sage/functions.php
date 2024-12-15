884680aa77b8db1a2af70ad54603b024352dfe6c
Igor Gorlov <gorlov35@gmail.com>
2024-12-15 20:39:46 +0300

    change dockerfile

--- wordpress/wp-content/themes/poolglass-sage/functions.php
+++ wordpress/wp-content/themes/poolglass-sage/functions.php
@@ -14,16 +14,12 @@
 |
 */
 
-if (!file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
-  wp_die(
-    __(
-      'Error locating autoloader. Please run <code>composer install</code>.',
-      'sage',
-    ),
-  );
+if (! file_exists($composer = __DIR__.'/vendor/autoload.php')) {
+  wp_die(__('Error locating autoloader. Please run <code>composer install</code>.', 'sage'));
 }
 
 require $composer;
+
 
 /*
 |--------------------------------------------------------------------------
